25bf1d6d3779cb8f43a815cbf9d8c56ee00862bd
Ari Vaniderstine <hack@ariari.io>
2016-05-05 13:56:08 -0400

    Simple box model with padding margin and border

---
+++ script.js
@@ -0,0 +1,11 @@
+$('#padding').change(function() {
+  $('#box').css('padding', $(this).val());
+});
+
+$('#margin').change(function() {
+  $('#box').css('margin', $(this).val());
+});
+
+$('#border').change(function() {
+  $('#box').css('border-width', $(this).val());
+});
